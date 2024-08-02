a2297f4c6ec6a4d4a3934314050dece6fdac5f1e
Mohammad Alsallal <malslal787@gmail.com>
2024-08-03 00:44:06 +0300

    Css kinda done

--- Portfolio/JS/main.js
+++ Portfolio/JS/main.js
@@ -1,7 +1,14 @@
 import "../style/general.css";
 import "../style/style.css";
 import "../style/header.css";
+import "../style/mobile__nav.css";
 import "../style/welcome.css";
 import "../style/About.css";
 import "../style/Projects.css";
+import "../style/ContactMe.css";
 import "../style/footer.css";
+
+import mobileNav from "./utils/mobile-nav";
+import BackTop from "./utils/BackToTop";
+mobileNav();
+BackTop();
---
+++ Portfolio/JS/utils/Dark_Theme.js
@@ -0,0 +1,1 @@
+const sun = document.getElementById("theme-toggle");
