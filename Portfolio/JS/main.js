71eb1f998bf1921b3787125bd2653202dd289957
Mohammad Alsallal <malslal787@gmail.com>
2024-08-02 19:46:45 +0300

    projects section is done

--- Portfolio/JS/main.js
+++ Portfolio/JS/main.js
@@ -3,3 +3,5 @@ import "../style/style.css";
 import "../style/header.css";
 import "../style/welcome.css";
 import "../style/About.css";
+import "../style/Projects.css";
+import "../style/footer.css";
